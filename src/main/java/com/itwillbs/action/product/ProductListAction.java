c667782712992e06ad0c7782c8cda45514e797f9
LCHova <lch1014l@naver.com>
2023-05-12 11:10:36 +0900

    Product: List fix
    fix         : 등록 페이지 수정
    			  리스트 중복 제거
    feat        : 조회수 기능 추가

--- src/main/java/com/itwillbs/action/product/ProductListAction.java
+++ src/main/java/com/itwillbs/action/product/ProductListAction.java
@@ -16,27 +16,28 @@ public class ProductListAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 		System.out.println("P : ProductListAction_execute() 호출");
-		
+
 		// ProductDAO 객체 생성
 		ProductDAO pdao = new ProductDAO();
-		
+
 		// 상품 정보 가져오기
 		List<ProductDTO> productList = pdao.getProductList();
-		        
+
 		// productList에서 랜덤으로 3개의 상품을 선택하여 새 리스트에 저장
 		List<ProductDTO> randomProductList = new ArrayList<>();
 		Random random = new Random();
 		int listSize = productList.size();
 		for (int i = 0; i < 3; i++) {
-		    int randomIndex = random.nextInt(listSize);
-		    randomProductList.add(productList.get(randomIndex));
+		    int randomIndex = random.nextInt(listSize - i); // 선택할 수 있는 인덱스 범위를 줄임
+		    ProductDTO randomProduct = productList.remove(randomIndex); // 선택된 상품을 productList에서 삭제(중복 방지 처리)
+		    randomProductList.add(randomProduct);
 		}
 
 		// request 객체에 저장
 		request.setAttribute("productList", randomProductList);
-		
+
 		System.out.println(productList.size());
 		System.out.println("");
 		// 연결된 view에 출력(./product/productList.jsp)
